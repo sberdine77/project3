34abeddbafd20c51af2fb04db496e8062b9896ce
Savio <sbsx77xavier@hotmail.com>
2016-12-02 23:13:25 -0300

    Test's fixing

--- project3.cpp
+++ project3.cpp
@@ -20,20 +20,7 @@
 #include <iostream>
 #include <string>
 #include <limits.h>
-#define E5 100000
-#define E6 1000000
-#define E7 10000000
-#define E8 100000000
-#define E9 1000000000
-#define E10 10000000000
-#define E11 100000000000
-#define E12 1000000000000
-#define E13 10000000000000
-#define E14 100000000000000
-#define E15 1000000000000000
-#define E16 10000000000000000
-#define E17 100000000000000000
-#define E18 1000000000000000000
+
 
 using namespace std;
 using std::string;
@@ -52,7 +39,7 @@ Counter::Counter(){
 
 //Returns the time elapsed since the criation in milliseconds
 double Counter::time_elapsed(){
-	return chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - begin).count()/1000.0;
+	return chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now() - begin).count()/1000000.0;
 }
 
 /***************************************************************************************************************************/
@@ -94,7 +81,7 @@ void quickSort(int arr[], int left, int right) {
                   i++;
                   j--;
             }
-      };
+      }
  
       /* recursion */
       if (left < j)
@@ -116,7 +103,7 @@ int getMax(int arr[], int n)
  */  
 void countSort(int arr[], int n, int exp)
 {
-    int output[n];
+   	int *output = new int[n];
     int i, count[10] = {0};
     for (i = 0; i < n; i++)
         count[(arr[i] / exp) % 10]++;
@@ -129,6 +116,7 @@ void countSort(int arr[], int n, int exp)
     }
     for (i = 0; i < n; i++)
         arr[i] = output[i];
+    delete[] output;
 }
 /* 
  * sorts arr[] of size n using Radix Sort
@@ -140,9 +128,9 @@ void radixsort(int arr[], int n)
         countSort(arr, n, exp);
 }
 
-long long int randomInt(long long int bigger, long long int smaller){
+int randomInt(int bigger, int smaller){
 	srand((unsigned)time(0));
-	long long int number = rand()%(bigger - smaller + 1) + smaller;
+	int number = rand()%(bigger - smaller + 1) + smaller;
 	return number;
 }
 
@@ -150,30 +138,35 @@ int main(int argc, char const *argv[]){
 
 	cout << "Generating arrays...\n";
 
-	int *v1 = new int[E7];
-	int *v2 = new int[E7];
-	int *v3 = new int[E7];
+	int *v1 = new int[1000000];
+	int *v2 = new int[1000000];
+	int *v3 = new int[100000000];
 
 	
 	/* v1 ordenação crescente, v2 ordenação decrescente*/
-	for (long long int i = 0; i < E7 /*10^5*/; i++){
+	for (int i = 0; i < 1000000 /*10^5*/; i++){
 		v1[i] = i;
-		v2[(E7 - 1) - i] = ((E7 - 1) - i);
+		v2[i] = (999999 -i);
+		
 	}
 
-	for (long long int i = 0; i < E7; i++){
-		v3[i] = randomInt(E7, 0);
+	for (int i = 0; i < 100000000; i++){
+ 		v3[i] = randomInt(99999999, 0);
 	}
-
+	cout << "wow \n";
 	Counter *cron;
 	cron = new Counter();
-	quickSort(v3, 0, E7);
+	cout << "wow2 \n";
+	quickSort(v3, 0, 100000000);
 	cout << cron->time_elapsed() << "\n";
 	delete cron;
 
 	delete[] v1;
+	
 	delete[] v2;
-	delete[] v2;
+
+	delete[] v3;
+
 
 	return 0;
 }
